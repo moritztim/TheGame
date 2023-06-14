87b18f0d628b499a17e031375ee92d8d00d3d204
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2023-06-15 01:37:44 +0200

    Doc

--- src/Card.ts
+++ src/Card.ts
@@ -1,18 +1,26 @@
 export class Card extends Object {
+	/** Maximum value of a card */
 	public static readonly MAX_VALUE = 99;
+	/** Minimum value of a card */
 	public static readonly MIN_VALUE = 2;
+
 	public readonly Value: number;
+
 	constructor(Value: number) {
 		super();
-		if (!(Value <= Card.MAX_VALUE && Value >= Card.MIN_VALUE)) {
+		if (!(Value <= Card.MAX_VALUE && Value >= Card.MIN_VALUE)) { // Out of bounds
 			throw new Error(`Card value must be from ${Card.MIN_VALUE} to ${Card.MAX_VALUE}, recieved ${Value}`);
 		}
 		this.Value = Value;
 	}
 
+	/**
+	 * @returns a string representation of the card with universal padding
+	 */
 	public override toString(): string {
-		return `[${this.Value.toString().padStart(Card.MAX_VALUE.toString().length, ' ')}]`;
+		return `[${this.Value.toString().padStart(Card.MAX_VALUE.toString().length, ' ')}]`; // Pad with spaces to make all cards the same length
 	}
+
 	public override valueOf(): number {
 		return this.Value;
 	}
