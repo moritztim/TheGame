85b899f0e7fb06b8f34d4a894954e4a258717374
Moritz T. W <moritz.widmann@stud.edubs.ch>
2023-06-29 23:41:41 +0200

    Hand Sizes Constant

--- src/Game.ts
+++ src/Game.ts
@@ -4,8 +4,17 @@ import { CardStack, Player, Direction } from './mod.ts';
  * The Game
  */
 export class Game {
+	// Constant Rules
+	// NOLI SE TANGERE
 	/** Maximum player count */
 	public static readonly MAX_PLAYERS = 5;
+	/** Hand sizes mapped to player counts */
+	public static readonly HAND_SIZES = new Map([
+		[1, 8],
+		[2, 7],
+		[3, 6] // 3 and above are the same
+	]);
+
 	/** Cards each player starts each round with (if there's enough cards in the @see Stack) */
 	public readonly HandSize: number;
 	/** Stack to draw from */
@@ -17,16 +26,8 @@ export class Game {
 	constructor(players: Player[]) {
 		if (players.length > Game.MAX_PLAYERS) { throw new Error(`Player limit (${Game.MAX_PLAYERS}) exceeded`); } // prevent overfill
 		this.players = players;
-		switch (players.length) {
-			case 1:
-				this.HandSize = 8;
-				break;
-			case 2:
-				this.HandSize = 7;
-				break;
-			default:
-				this.HandSize = 6;
-		}
+		this.HandSize = Game.HAND_SIZES.get(players.length) || Game.HAND_SIZES.get(Game.HAND_SIZES.size) as number // get the hand size for the player count (or the last one if it doesn't exist)
+
 		this.DrawPile = new CardStack();
 		const slotsBuilder: CardStack[] = [];
 		for (const keyString in Direction) {
